b27619d9284976f225634bf64ca35a5b4d2892f0
janakipriya23 <127486140+janakipriya23@users.noreply.github.com>
2023-03-10 15:51:43 +0530

    Add files via upload

---
+++ program1.java
@@ -0,0 +1,11 @@
+package com.testingautomation;
+
+public class program1 {
+
+    public static void main(String[] args)
+
+    {
+        System.out.println("welcome to testing automation classes");
+
+    }
+}
